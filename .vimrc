c70285a18b5d0e270168605dd31f8e0b17ef96de
twktheainur <twk.theainur@gmail.com>
2017-01-03 14:21:26 +0100

    Added installation script. Fixes in vimrc for clipboard interoperability on osx

--- .vimrc
+++ .vimrc
@@ -7,5 +7,12 @@ source ~/.vim_runtime/vimrcs/extended.vim
 
 try
 source ~/.vim_runtime/my_configs.vim
+vnoremap \y y:call system("pbcopy", getreg("\""))<CR>
+nnoremap \p :call setreg("\"", system("pbpaste"))<CR>p
+
+noremap YY "+y<CR>
+noremap P "+gP<CR>
+noremap XX "+x<CR>
 catch
 endtry
+
---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#Install Brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+#Install Cask
+brew tap caskroom/cask
+
+#Installing zsh 
+brew install zsh
+
+#Installing utilities
+brew install wget zsh grep gnu-sed gnu-getopt gnupg2 vim
+
+#Retrieving oh-my-zsh
+pushd $HOME
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+popd
+
+#Copying custom theme
+cp agnoster-twk.zsh-theme $HOME/.oh-my-zsh/themes/
+
+#Copyting configuration 
+cp .zshrc $HOME/
+
+#Copying VIM configuration
+cp -r .vim_runtime/ $HOME/
+cp .vimrc $HOME/
+
+for i in `ls -1 .fonts/*/*.ttf` do
+    open $i
+done
